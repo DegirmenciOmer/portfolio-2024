a8d4bfaecafea009e30220435e7aba9c741e2ed6
DegirmenciOmer <rumidegirmenci0@gmail.com>
2024-06-22 22:00:28 +0200

    initial setup

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1>Frontend Web Developer</h1>
+      <h2>Hi, my name is Ömer</h2>
+    </section>
+  )
+}
+
+export default HeroSection
